05e6a95c40ecbb54dec25ed975c66e67aa315be0
CUPOKriot <57013986+CUPOKriot@users.noreply.github.com>
2023-08-26 17:06:02 +0300

    Сверстана шапка и первая секция

---
+++ js/logo.js
@@ -0,0 +1,7 @@
+// const text = document.querySelector('.header__logo-text');
+// text.innerHTML = text.textContent.replace(/\S/g, '<span class="header__logo-item">$&</span>');
+
+// const element = document.getElementsByClassName('header__logo-item');
+// for (let i = 0; i < element.length; i++) {
+//     element[i].style.transform = "rotate(" + i*11.3 + "deg)"
+// }
